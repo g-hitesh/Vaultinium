c267c7ea0063d9b08aaa8b1a60e607c34e58c010
Hitesh Kumar <hitesh2662006@gmail.com>
2025-07-20 18:07:27 +0530

    Some css fixes

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -59,20 +59,20 @@ const Manager = () => {
     return (
         <>
             <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-            <div className=" mx-auto max-w-4xl container px-40">
+            <div className="mx-auto  max-w-4xl min-h-[calc(100vh-61.39)] container px-40  md:px-0">
                 <h1 className='text-4xl font-bold text-center text-[#e4cb7b]'>Vaultinium</h1>
                 <p className='text-[#d8b74a] text-lg text-center'>Your Password Manager</p>
                 <div className='text-white flex flex-col p-4 gap-8 items-center'>
                     <input value={form.site} onChange={handleChange} placeholder='Enter Website URL' className=' border-2 w-full border-[#e4cb7b] rounded-2xl px-4 py-1' type="text" name='site' />
-                    <div className="flex w-full  justify-between gap-8">
+                    <div className="flex w-full flex-col md:flex-row justify-between gap-8">
                         <input value={form.username} onChange={handleChange} placeholder='Enter Username' className=' border-2 border-[#e4cb7b] rounded-2xl px-4 py-1' type="text" name='username' />
                         <div className="relative ">
                             <input value={form.password} onChange={handleChange} placeholder='Enter Password' className=' border-2 border-[#e4cb7b] rounded-2xl px-4 py-1 w-full' type={passType} name='password' />
                             <span onClick={showPass} className="absolute right-2 top-2 cursor-pointer">{showPassword ? <FaEye /> : <FaEyeSlash />}</span>
                         </div>
                     </div>
-                    {/* disabled={form.password.length < 6 && form.username.length < 3 && form.site.length < 4} */}
-                    <button onClick={savePassword} className='text-black flex justify-center items-center bg-[#e4cb7b] rounded-full gap-2 px-8 py-2 w-fit cursor-pointer border-2 border-[#f2c01a]'>
+
+                    <button disabled={form.password.length < 6 && form.username.length < 3 && form.site.length < 4} onClick={savePassword} className='text-black flex justify-center items-center bg-[#e4cb7b] rounded-full gap-2 px-8 py-2 w-fit cursor-pointer border-2 border-[#f2c01a]'>
                         <RiApps2AddFill />
                         Save Pass</button>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className="sticky top-0">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center md:justify-between justify-center mx-auto p-3">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/src/assets/logo.png" className="w-[200px]" alt="Vaultinium Logo" />
                 </a>
-                <div className="items-center justify-between flex w-auto order-1" id="navbar-language">
-                    <ul className="flex font-medium p-0 rounded-lg space-x-2 rtl:space-x-reverse flex-row mt-0 border-0">
-                        <li>
-                            <a href="#home" className="block py-2 px-3 text-white hover:text-[#e4cb7b] rounded-sm bg-transparent p-0" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="#about" className="block py-2 px-3 text-white hover:text-[#e4cb7b] rounded-sm bg-transparent p-0">About</a>
-                        </li>
-                        <li>
-                            <a href="#contact" className="block py-2 px-3 text-white hover:text-[#e4cb7b] rounded-sm bg-transparent p-0">Contact</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     )
