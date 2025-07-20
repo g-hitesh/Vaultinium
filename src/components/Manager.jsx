109d7a890fdebbc02b65bbd640029c7f30f5462c
Hitesh Kumar <hitesh2662006@gmail.com>
2025-07-20 17:27:10 +0530

    Added edit and delete buttons

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { RiApps2AddFill } from "react-icons/ri";
-import { FaEyeSlash, FaEye } from "react-icons/fa";
+import { FaEyeSlash, FaEye, FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import { v4 as uuidv4 } from 'uuid';
+
 const Manager = () => {
     const [passType, setpassType] = useState("password")
     const [showPassword, setshowPassword] = useState(false)
-    const [form, setform] = useState({ site: "", username: "", password: "" })
+    const [form, setForm] = useState({ site: "", username: "", password: "" })
     const [passwordArray, setpasswordArray] = useState([])
 
     useEffect(() => {
@@ -15,26 +18,41 @@ const Manager = () => {
         }
     }, [])
 
+    // const handleButtonClick = () => {
+    //     if (inputRef) {
+    //         inputRef.current.focus();
+    //     }
+    // };
+
+
 
     const showPass = () => {
-        if(passType==="password"){
-        setpassType("text")
+        if (passType === "password") {
+            setpassType("text")
         }
-        else{
+        else {
             setpassType("password")
         }
         setshowPassword(!showPassword)
     }
 
     const savePassword = () => {
-        setpasswordArray([...passwordArray, form])
-        localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
-        console.log(passwordArray)
+        setpasswordArray([...passwordArray, { ...form, id: uuidv4() }])
+        localStorage.setItem("passwords", JSON.stringify([...passwordArray, { ...form, id: uuidv4() }]))
+        setForm({ site: "", username: "", password: "" })
+    }
+    const deletePassword = (id) => {
+        setpasswordArray(passwordArray.filter(i => i.id !== id))
+        localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(i => i.id !== id)))
+    }
 
+    const editPassword = (id) => {
+        setForm(passwordArray.filter(i => i.id === id)[0]);
+        setpasswordArray(passwordArray.filter(i => i.id !== id));
     }
 
     const handleChange = (e) => {
-        setform({ ...form, [e.target.name]: e.target.value })
+        setForm({ ...form, [e.target.name]: e.target.value })
     }
 
 
@@ -53,18 +71,19 @@ const Manager = () => {
                             <span onClick={showPass} className="absolute right-2 top-2 cursor-pointer">{showPassword ? <FaEye /> : <FaEyeSlash />}</span>
                         </div>
                     </div>
+                    {/* disabled={form.password.length < 6 && form.username.length < 3 && form.site.length < 4} */}
                     <button onClick={savePassword} className='text-black flex justify-center items-center bg-[#e4cb7b] rounded-full gap-2 px-8 py-2 w-fit cursor-pointer border-2 border-[#f2c01a]'>
                         <RiApps2AddFill />
-                        Add Pass</button>
+                        Save Pass</button>
                 </div>
-                <div className="passwords">
-                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <caption className="p-5 w-[15vw] text-lg font-semibold text-left rtl:text-right text-[#e4cb7b] ">
-                            Your Passwords
-                        </caption>
+                <div className="passwords  ">
+                    <caption className="p-5 w-[15vw] text-lg font-semibold text-left rtl:text-right text-[#e4cb7b] ">
+                        Your Passwords
+                    </caption>
+                    <div className="relative h-[300px] overflow-x-auto overflow-y-auto shadow-md sm:rounded-lg scrollbar-dark">
                         {passwordArray.length === 0 && <div className='text-white py-5 text-center'>No Passwords to Display</div>}
                         {passwordArray.length != 0 &&
-                            <table className="w-full text-sm rounded-sm text-left rtl:text-right text-gray-500 ">
+                            <table className="w-full text-sm rounded-sm text-left rtl:text-right text-gray-500 overflow-y-scroll">
                                 <thead className="text-xs text-gray-700 uppercase  bg-[#e4cb7b] ">
                                     <tr>
                                         <th scope="col" className="px-6 py-3">
@@ -76,14 +95,21 @@ const Manager = () => {
                                         <th scope="col" className="px-6 py-3">
                                             Password
                                         </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Actions
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {passwordArray.map((item, index) => {
                                         return (<tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"><a href={item.site} target='_blank'>{item.site}</a></th>
-                                            <td className="px-6 py-4 w-32">{item.username}</td>
-                                            <td className="px-6 py-4 w-32">{item.password}</td>
+                                            <td className="px-6 py-4 ">{item.username}</td>
+                                            <td className="px-6 py-4 ">{item.password}</td>
+                                            <td className="px-6 py-4  flex gap-3 justify-center items-center">
+                                                <button onClick={() => { editPassword(item.id) }} className='cursor-pointer'><FaEdit size={18} /></button>
+                                                <button onClick={() => { deletePassword(item.id) }} className='cursor-pointer'><MdDeleteForever size={18} /></button>
+                                            </td>
                                         </tr>
                                         )
                                     })}
