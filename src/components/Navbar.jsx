d73987f6687116aef291d543dbda04d74c67fbae
Hitesh Kumar <hitesh2662006@gmail.com>
2025-07-21 15:50:34 +0530

    fixed a bug for logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="sticky top-0">
             <div className="max-w-screen-xl flex flex-wrap items-center md:justify-between justify-center mx-auto p-3">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/src/assets/logo.png" className="w-[200px]" alt="Vaultinium Logo" />
+                    <img src="/logo.png" className="w-[200px]" alt="Vaultinium Logo" />
                 </a>
             </div>
         </nav>
