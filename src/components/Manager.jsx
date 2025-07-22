9646b50afce3b24e0bd0111f9a25681be0835c5d
Hitesh Kumar <hitesh2662006@gmail.com>
2025-07-22 17:37:03 +0530

    more design fixes

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -58,7 +58,6 @@ const Manager = () => {
 
     return (
         <>
-            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             <div className="mx-auto flex flex-col items-center  md:max-w-4xl min-h-[calc(100vh-61.39)] container mt-8 md:mt-2  md:px-0">
                 <h1 className='text-4xl font-bold text-center text-[#e4cb7b]'>Vaultinium</h1>
                 <p className='text-[#d8b74a] text-lg text-center'>Your Password Manager</p>
